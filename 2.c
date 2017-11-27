d0e1259b67a81d653c3101b2b7a1d887c0f589eb
huanhuanwang <33029015+wiesswang@users.noreply.github.com>
2017-11-27 20:47:47 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,14 +1,13 @@
 # include<stdio.h>
 # include<math.h>
-# include<stdlib.h>
-#include "omp.h"
+# include "omp.h"
 int main()
 {
-	int m =1;
-	for (m=1;m<9;m++){
-		double start = omp_get_wtime( );
-		int i,j,k=0;
-		int n=0;
+	int m =1, x = 0;
+	for (m=1;m<9;m++)
+	{
+		int i,j;
+		int k=0;
 		FILE *fp;
 		fp = fopen ("result.txt","w");
 		if (fp ==NULL)
@@ -16,24 +15,34 @@ int main()
 			printf ("faile open the file!\n");
 			exit (-1);
 		}
-		omp_set_num_threads(m);	
-		for(i=2;i<100000;i++){
-			for(j=2;j<=sqrt(i);j++)
-				if(i%j==0)
-					break; 
-				if(j>sqrt(i))
-        			{
-					fprintf (fp,"%d ",i);
-            				k++;
-        			}
-    		}
-		printf("\n素数的个数为:%d\n",k);
-		printf("\n线程数为:%d\n",m);
-    		double end = omp_get_wtime( );   
-    		printf("start = %.16g\nend = %.16g\ndiff = %.16g seconds\n",  
-             		start, end, end - start);  
-  
+		double start = omp_get_wtime( );
+		omp_set_num_threads(m);
+		#pragma omp parallel for  private(i,j) reduction(+:k)
+		for (i = 2; i < 100000; i++)
+       		{
+			x = omp_get_num_threads();
+			for (j = 2; j <= sqrt(i); j++)
+               		{
+                       		if (i % j == 0)
+                       		{
+                               		break;
+                       		}
+               		}
+               		if(j > sqrt(i))
+               		{
+                       		fprintf (fp,"%d ",i);
+				k++;
+                        }
+		}
+		printf("素数的个数为:%d ",k);
+	        printf("线程数为:%d ",x);
+   		double end = omp_get_wtime( );  
+   		printf("start = %.16g,end = %.16g,diff = %.16g seconds ",start, end, end - start);  
+		//fclose(fp)
+		double s = 0.019/(end - start);
+		printf("加速比为:%f\n",s);
 	}
-
+	return 0;
 }
+
 
